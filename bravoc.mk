869f34891f8945fea78f8abf337a3ed8cc512291
Corey Moyer <corey@CronBox4-Dev.(none)>
2012-10-07 23:17:51 -0400

    Cleanup

--- bravoc.mk
+++ bravoc.mk
@@ -19,7 +19,7 @@
 # not specialized for any geography.
 #
 
-# First, the most specific values, i.e. the aspects that are specific to GSM
+# First, the most specific values, i.e. the aspects that are specific to CDMA
 
 DEVICE_PACKAGE_OVERLAYS := device/htc/bravoc/overlay
 
@@ -77,29 +77,29 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    device/htc/bravoc/init.bravoc.rc:root/init.bravoc.rc \
-    device/htc/bravoc/init.bravoc.usb.rc:root/init.bravoc.usb.rc \
-    device/htc/bravoc/ueventd.bravoc.rc:root/ueventd.bravoc.rc
+    device/htc/bravoc/prebuilt/init.bravoc.rc:root/init.bravoc.rc \
+    device/htc/bravoc/prebuilt/init.bravoc.usb.rc:root/init.bravoc.usb.rc \
+    device/htc/bravoc/prebuilt/ueventd.bravoc.rc:root/ueventd.bravoc.rc
 
 # Prebuilt files/configs
 PRODUCT_COPY_FILES += \
-    device/htc/bravoc/bravoc-keypad.kl:system/usr/keylayout/bravoc-keypad.kl \
-    device/htc/bravoc/bravoc-keypad.kcm:system/usr/keychars/bravoc-keypad.kcm \
-    device/htc/bravoc/curcial-oj.idc:system/usr/idc/curcial-oj.idc \
-    device/htc/bravoc/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-    device/htc/bravoc/h2w_headset.kcm:system/usr/keychars/h2w_headset.kcm \
-    device/htc/bravoc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
-    device/htc/bravoc/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
-    device/htc/bravoc/synaptics-rmi-touchscreen.kcm:system/usr/keychars/synaptics-rmi-touchscreen.kcm \
-    device/htc/bravoc/vold.fstab:system/etc/vold.fstab \
-    device/htc/bravoc/sysctl.conf:system/etc/sysctl.conf
+    device/htc/bravoc/prebuilt/bravoc-keypad.kl:system/usr/keylayout/bravoc-keypad.kl \
+    device/htc/bravoc/prebuilt/bravoc-keypad.kcm:system/usr/keychars/bravoc-keypad.kcm \
+    device/htc/bravoc/prebuilt/curcial-oj.idc:system/usr/idc/curcial-oj.idc \
+    device/htc/bravoc/prebuilt/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/bravoc/prebuilt/h2w_headset.kcm:system/usr/keychars/h2w_headset.kcm \
+    device/htc/bravoc/prebuilt/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    device/htc/bravoc/prebuilt/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
+    device/htc/bravoc/prebuilt/synaptics-rmi-touchscreen.kcm:system/usr/keychars/synaptics-rmi-touchscreen.kcm \
+    device/htc/bravoc/prebuilt/vold.fstab:system/etc/vold.fstab \
+    device/htc/bravoc/prebuilt/sysctl.conf:system/etc/sysctl.conf
 
 # media config xml file
 PRODUCT_COPY_FILES += \
-    device/htc/bravoc/media_profiles.xml:system/etc/media_profiles.xml
+    device/htc/bravoc/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
 
 # Common qsd8k stuff
 $(call inherit-product, device/htc/qsd8k-common/qsd8k.mk)
 
-# Also get non-open-source GSM-specific aspects if available
+# Also get non-open-source CDMA-specific aspects if available
 $(call inherit-product-if-exists, vendor/htc/bravoc/bravoc-vendor.mk)
