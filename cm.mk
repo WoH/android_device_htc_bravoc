ba2454fdbd510e5a6bcf3b30c3c298988c28c29a
Corey Moyer <corey@CronBox4-Dev.(none)>
2012-10-17 09:07:51 -0400

    Quick fix

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Setup device specific product configuration.
 #
 PRODUCT_NAME    := cm_bravoc
-PRODUCT_BRAND   := htc_wwe
+PRODUCT_BRAND   := us_cellular_wwe
 PRODUCT_DEVICE  := bravoc
 PRODUCT_MODEL   := HTC Desire CDMA
 PRODUCT_MANUFACTURER := HTC
@@ -19,4 +19,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=htc_wwe/htc_bravo/bravo:2.3.3/GRI40/96875.1:user/release-keys \
     BUILD_VERSION_TAGS=release-keys \
     PRIVATE_BUILD_DESC="3.14.405.1 CL96875 release-keys" \
-    PRODUCT_NAME=bravoc
+    PRODUCT_NAME=htc_bravo
