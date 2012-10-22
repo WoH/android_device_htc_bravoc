d9bccfe893007dd4e817b2f904f4d3f820b592c7
Corey Moyer <corey@CronBox4-Dev.(none)>
2012-10-22 00:40:41 -0400

    use new build config

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/cdma.mk)
 $(call inherit-product, device/htc/bravoc/full_bravoc.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_cron_phone.mk)
 
 #
 # Setup device specific product configuration.
